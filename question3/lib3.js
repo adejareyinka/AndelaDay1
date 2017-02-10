85ac430831cafb4372cce8c71aa33f73eb3126ae
unknown <jareyinka@gmail.com>
2017-02-09 19:14:19 -0800

    question 2 refactored code result updated

--- question3/lib3.js
+++ question3/lib3.js
@@ -1,16 +1,16 @@
 module.exports={
 	aritGeo:function(arr){
-        var ap, gp;
+        var arith, geom;
         if (arr.length==0){return 0;}
         for (i = 0; i< (arr.length - 2); i++)  
-            if(!(ap = arr[i+1] - arr[i] == arr[i+2] - arr[i+1])) break; 
+            if(!(arith = arr[i+1] - arr[i] == arr[i+2] - arr[i+1])) break; 
 
-        if(ap) return "Arithmetic";
+        if(arith) return "Arithmetic";
 
         for (i = 0; i< (arr.length - 2); i++)  
-            if(!(gp = arr[i+1] / arr[i] == arr[i+2] / arr[i+1])) break;
+            if(!(geom = arr[i+1] / arr[i] == arr[i+2] / arr[i+1])) break;
 
-        if(gp) return "Geometric";
+        if(geom) return "Geometric";
 
         return -1;    
        
