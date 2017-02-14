2981ddc64128f6481e3af376e367402fe983d46b
unknown <jareyinka@gmail.com>
2017-02-13 23:35:12 -0800

    working code for question 3

--- day1/question3/code.js
+++ day1/question3/code.js
@@ -1,19 +1,17 @@
 module.exports={
-	Person: function(firstName, lastName, age, isMr){
+	Person: function(firstName, lastName, isMr){
 		var prefix = isMr ? "Mr." : "Mrs." ;
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.age = function(age){
-		if(age >= 0 && age <18){
-			return 'You are below 18 years old';
-		}else if(age <= 150){
-			return 'You are an adult';
-		}else 
-			return 'you can not be over 150 years';
-		}
-	this.getFullName = function(){
-	 	return prefix + " "  + firstName + " " + lastName + ". " + age;
-	 }
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.wage = 0;
+		this.salary = salary;
+		this.getFullName = function(){
+	 		return prefix + " "  + firstName + " " + lastName + ".";
+	 	}
+		function salary(pay){
+			with(this){
+				wage = pay;
+			}
+		}		
 	}
-
 }
